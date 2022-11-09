2b818ca91b85311935ba18c449dff0c0e950a148
//dkp <58055667+davekpatrick@users.noreply.github.com>
2022-11-09 12:33:16 -0500

    initial commit (#3)

---
+++ java/helloWorld.java
@@ -0,0 +1,11 @@
+/*
+ * Token hello world program, thank you Brian Kernighan 
+ */
+//BOF
+public class helloWorld {
+  //
+  public static void main(String[] args) {
+    System.out.println("Hello, World!");
+  }
+}
+//EOF
